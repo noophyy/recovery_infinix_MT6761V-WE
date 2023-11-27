762040ce71efadcc1eea1d7164e2d3f3d15dfe9d
Massatrio16 <dimassetosatrio@gmail.com>
2023-11-27 19:18:37 +0700

    first commit

---
+++ device.mk
@@ -0,0 +1,27 @@
+LOCAL_PATH := device/infinix/X657B
+
+PRODUCT_TARGET_VNDK_VERSION := 29
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctrl.mt6761
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+# Dynamic Partition
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd \
+    android.hardware.fastboot@1.0-impl-mtk.so
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+
---
+++ twrp_X657B.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from spinel device
+$(call inherit-product, device/infinix/X657B/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := X657B
+PRODUCT_NAME := twrp_X657B
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix X657B
+PRODUCT_MANUFACTURER := infinix
+PRODUCT_RELEASE_NAME := Infinix Infinix X657B
+
