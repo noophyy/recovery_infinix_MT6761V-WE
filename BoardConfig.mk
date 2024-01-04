52a713558191521752271b0e8b012381f22cb0a3
Massatrio16 <146039181+Massatrio16@users.noreply.github.com>
2024-01-04 17:03:16 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -112,7 +112,8 @@ $(TARGET_OUT_VENDOR_EXECUTABLES)/teei_daemon \
     $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/libkeymaster3device.so \
 $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
     $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw/vendor.mediatek.hardware.keymaster_attestation@1.1-impl.so \
-    $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/vendor.mediatek.hardware.keymaster_attestation@1.0.so 
+    $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/vendor.mediatek.hardware.keymaster_attestation@1.0.so \
+$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw/android.hardware.keymaster@3.0-impl.so
     
     
     
