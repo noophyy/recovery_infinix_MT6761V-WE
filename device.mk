76247ef3def61482f199f044f4e27add3540a4ee
Massatrio16 <146039181+Massatrio16@users.noreply.github.com>
2024-02-26 14:34:42 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -3,6 +3,16 @@ LOCAL_PATH := device/infinix/X657B
 PRODUCT_TARGET_VNDK_VERSION := 29
 PRODUCT_SHIPPING_API_LEVEL := 29
 
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-impl-recovery
+
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service \
+    libhealthd.$(PRODUCT_PLATFORM)
+
+
 PRODUCT_PACKAGES_DEBUG += \
     bootctrl.mt6761
 
